6ff6abdfd362cf06412ea3ed9f4bac2e1b13f197
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-02-24 14:35:41 +0200

    Initialize project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h2>Using CKEditor&nbsp;5 build in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event ) => {
-                        console.log( event );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
