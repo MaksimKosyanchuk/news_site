ba3415bfc4ba2e8ff2af67728caf327324c4d367
MaksimKosyanchuk <m.o.kosianchuk@student.khai.edu>
2024-02-24 18:56:54 +0200

    Added Header, Footer, StartScreen models

---
+++ frontend/src/models/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+      <header className="header blurred">
+        <div className="header_content">
+          <h1 className="header_title">News Site</h1>
+        </div>
+      </header>
+    );
+}
+
+export default Header;
---
+++ frontend/src/models/StartScreen/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+function StartScreen() {
+    return (
+        <div className="start-screen">
+           <div className="main-div">
+           
+           </div>
+        </div>
+    );
+}
+
+export default StartScreen;
